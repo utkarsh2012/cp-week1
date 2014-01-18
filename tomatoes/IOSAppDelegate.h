a2b5f7c4c0be1dd434e3a4e4e53feb2658f77958
Utkarsh Sengar <utkarsh2012@gmail.com>
2014-01-17 23:06:39 -0800

    Initial Commit

---
+++ tomatoes/IOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IOSAppDelegate.h
+//  tomatoes
+//
+//  Created by Utkarsh Sengar on 1/17/14.
+//  Copyright (c) 2014 area42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatoes/IOSViewController.h
@@ -0,0 +1,13 @@
+//
+//  IOSViewController.h
+//  tomatoes
+//
+//  Created by Utkarsh Sengar on 1/17/14.
+//  Copyright (c) 2014 area42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSViewController : UIViewController
+
+@end
