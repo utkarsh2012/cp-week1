ec9693ceae819b35eb645d789e681f083c0aad46
Utkarsh Sengar <utkarsh2012@gmail.com>
2014-01-20 14:06:56 -0800

    More bugfixes

--- tomatoes/MoviesViewController.m
+++ tomatoes/MoviesViewController.m
@@ -12,6 +12,7 @@
 #import "Movie.h"
 #import "UIImageView+AFNetworking.h"
 #import "Reachability.h"
+#import "SVProgressHUD.h"
 
 @interface MoviesViewController ()
 {
@@ -44,8 +45,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"Movies";
     [self testInternetConnection];
+    self.title = @"Movies";
     UIRefreshControl *refresh = [[UIRefreshControl alloc] init];
     
     refresh.attributedTitle = [[NSAttributedString alloc] initWithString:@"Pull to Refresh"];
@@ -55,8 +56,8 @@
     [self reload];
 }
 
-- (void)stopRefresh {
-    [self.refreshControl endRefreshing];
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return YES;
 }
 
 - (int)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -73,12 +74,13 @@
     
     NSURL *url = [[NSURL alloc] initWithString:movie.image];
     [cell.movieImage setImageWithURL:url placeholderImage:[UIImage imageNamed:@"placeholder"]];
-
+    
     return cell;
 }
 
 
 - (void) reload{
+    [self showWithStatus];
     NSString *url = @"http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=g9au4hv6khv6wzvzgt55gpqs";
     
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
@@ -104,7 +106,6 @@
             [movie_holder setObject:movie[@"title"] forKey:@"title"];
             [movie_holder setObject:movie[@"synopsis"] forKey:@"synopsis"];
             [movie_holder setObject:movie[@"posters"][@"original"] forKey:@"image"];
-            //[movie_holder setObject:@"http://www.williamhadams.com/wp-content/uploads/2013/06/Superman-Poster.jpg" forKey:@"image"];
             Movie *movie_obj = [[Movie alloc] initWithDictionary:movie_holder];
             [movies addObject:movie_obj];
         }
@@ -112,6 +113,7 @@
         self.movies = movies;
         [self.tableView reloadData];
         [self performSelector:@selector(stopRefresh) withObject:nil afterDelay:2.5];
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:1];
     }];
 }
 
@@ -124,30 +126,71 @@
     movieDetailViewController.movie = movie;
 }
 
+- (void)stopRefresh {
+    [self.refreshControl endRefreshing];
+}
+
 // Checks if we have an internet connection or not
 - (void)testInternetConnection
 {
     internetReachableFoo = [Reachability reachabilityWithHostname:@"www.google.com"];
-    
-    // Internet is reachable
     internetReachableFoo.reachableBlock = ^(Reachability*reach)
     {
-        // Update the UI on the main thread
         dispatch_async(dispatch_get_main_queue(), ^{
             NSLog(@"Yayyy, we have the interwebs!");
         });
     };
-    
-    // Internet is not reachable
     internetReachableFoo.unreachableBlock = ^(Reachability*reach)
     {
-        // Update the UI on the main thread
         dispatch_async(dispatch_get_main_queue(), ^{
             NSLog(@"Someone broke the internet :(");
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Important" message: @"You are not connected to the internet.  Please try again." delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
         });
     };
     
     [internetReachableFoo startNotifier];
+}
+
+#pragma mark -
+#pragma mark Show Methods Sample
+
+- (void)showWithStatus {
+    [SVProgressHUD showWithStatus:@"Doing Stuff" ];
+}
+
+static float progress = 0.0f;
+
+- (IBAction)showWithProgress:(id)sender {
+    progress = 0.0f;
+    [SVProgressHUD showProgress:0 status:@"Loading"];
+    [self performSelector:@selector(increaseProgress) withObject:nil afterDelay:0.3];
+}
+
+- (void)increaseProgress {
+    progress+=0.1f;
+    [SVProgressHUD showProgress:progress status:@"Loading"];
+    
+    if(progress < 1.0f)
+        [self performSelector:@selector(increaseProgress) withObject:nil afterDelay:0.3];
+    else
+        [self performSelector:@selector(dismiss) withObject:nil afterDelay:0.4f];
+}
+
+
+#pragma mark -
+#pragma mark Dismiss Methods Sample
+
+- (void)dismiss {
+    [SVProgressHUD dismiss];
+}
+
+- (void)dismissSuccess {
+    [SVProgressHUD showSuccessWithStatus:@"Great Success!"];
+}
+
+- (void)dismissError {
+    [SVProgressHUD showErrorWithStatus:@"Failed with Error"];
 }
 
 @end
